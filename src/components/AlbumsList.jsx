ffe41295672a42d540c85682a94fda0a126b37b9
Ian M. Nava Garduno <ianmng1@gmail.com>
2024-11-06 07:26:57 -0600

    Creation of ExpandableP & AlbumList to display on every user

---
+++ src/components/AlbumsList.jsx
@@ -0,0 +1,5 @@
+function AlbumsList({ user }) {
+  return <div>Album list of {user.name}</div>;
+}
+
+export default AlbumsList;
--- src/components/UsersListItem.jsx
+++ src/components/UsersListItem.jsx
@@ -2,22 +2,27 @@ import { GoTrashcan } from "react-icons/go";
 import { removeUser } from "../store";
 import { useThunk } from "../hooks/useThunk";
 import Button from "./Button";
+import ExpandablePanel from "./ExpandablePanel";
+import AlbumsList from "./AlbumsList";
 
 function UsersListItem({ user }) {
   const [doRemoveUser, isLoading, error] = useThunk(removeUser);
 
+  const header = (
+    <>
+      {" "}
+      <Button className="mr-3" onClick={() => doRemoveUser(user)}>
+        {GoTrashcan}
+      </Button>
+      {error && <p>Error deleting user</p>}
+      {user.name}
+    </>
+  );
+
   return (
-    <div className="mb-2 border rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
-        <div className="flex flex-row items-center justify-between">
-          <Button className="mr-3" onClick={() => doRemoveUser(user)}>
-            {GoTrashcan}
-          </Button>
-          {error && <p>Error deleting user</p>}
-          {user.name}
-        </div>
-      </div>
-    </div>
+    <ExpandablePanel header={header}>
+      <AlbumsList user={user} />
+    </ExpandablePanel>
   );
 }
 
